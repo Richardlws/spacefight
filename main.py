4d8af95ee0b512a1013ee5781013cf99383a0e59
Richardlws <liu.bing66066@hotmail.com>
2025-03-11 17:25:03 +0800

    init

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import pygame
 
+WIDTH,HEIGHT = 900,600
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("SPACE Fight Game!")
+
+def main():
+    is_running = True
+    while is_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
